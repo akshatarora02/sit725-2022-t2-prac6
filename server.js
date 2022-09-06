bdfa3c632cb4d92424775357cf7cb1920a60f4bd
akshatarora02 <akshatarora028@gmail.com>
2022-09-06 22:13:46 +1000

    Added test.js file and addition code to server.js

--- server.js
+++ server.js
@@ -12,6 +12,23 @@ app.use(cors())
 app.use("/api/projects", projectRoutes)
 app.use("/api/user", userRoutes)
 
+const addNumbers = (number1, number2) => {
+    var num1 = parseInt(number1)
+    var num2 = parseInt(number2)
+    var result = (num1 + num2) || null;
+    return result;
+}
+
+app.get("/addTwoNumbers/:firstNumber/:secondNumber", (req, res) => {
+    var number1 = req.params.firstNumber;
+    var number2 = req.params.secondNumber;
+    var result = addNumbers(number1, number2)
+    if (result == null) {
+        res.json({ result: result, statusCode: 400 }).status(400)
+    }
+    else { res.json({ result: result, statusCode: 200 }).status(200) }
+})
+
 const port = process.env.port || 3000;
 
 app.listen(port, () => {
